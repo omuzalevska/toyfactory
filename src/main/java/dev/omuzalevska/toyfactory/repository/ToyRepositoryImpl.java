aa75755e63291974803e1495a25144403fef5574
OMuzalevska <oksana.muzalevskaja@gmail.com>
2024-11-22 15:41:54 +0100

    change readme

--- src/main/java/dev/omuzalevska/toyfactory/repository/ToyRepositoryImpl.java
+++ src/main/java/dev/omuzalevska/toyfactory/repository/ToyRepositoryImpl.java
@@ -1,29 +1,24 @@
 package dev.omuzalevska.toyfactory.repository;
 
 import dev.omuzalevska.toyfactory.model.Toy;
-
 import java.util.ArrayList;
 import java.util.List;
 
 public class ToyRepositoryImpl implements ToyRepository {
     private final List<Toy> toys = new ArrayList<>();
 
-    @Override
     public void add(Toy toy) {
         toys.add(toy);
     }
 
-    @Override
     public boolean removeById(String id) {
         return toys.removeIf(toy -> toy.getId().equals(id));
     }
 
-    @Override
     public List<Toy> getAll() {
         return new ArrayList<>(toys); // Захист від модифікації зовнішнім кодом
     }
 
-    @Override
     public Toy findById(String id) {
         return toys.stream()
                 .filter(toy -> toy.getId().equals(id))
